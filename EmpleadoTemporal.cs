f3e49dda48277c24434b256eceb0ff8eb4fb215b
art1221 <arturo.rojas@utpuebla.edu.mx>
2025-06-02 11:18:46 -0600

    Se agregaron las 2 nuevas clases

---
+++ EmpleadoTemporal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W
+{
+    internal class EmpleadoTemporal : Empleado
+    {
+        public int HorasExtra { get; set; }
+        public override double CalcularSueldo()
+        {
+            return SueldoBase + (HorasExtra * 100);
+        }
+    }
+}
